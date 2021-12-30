6412af212ddf3c84db4f443f4b1cb0580d60d696
Paul Chambaz <paul.chambazz@gmail.com>
2021-12-30 02:05:27 +0100

    Added compiler optimization

--- build/config.mk
+++ build/config.mk
@@ -5,7 +5,7 @@ CONFPREFIX = .config
 TERMINFO := ${DESTDIR}${PREFIX}/share/terminfo
 INCS = -I ../include
 LIBS = -lcurl
-CFLAGS += -std=c99 ${INCS} -DVERSION=\"${VERSION}\" -DNDBUG
+CFLAGS += -std=c99 ${INCS} -DVERSION=\"${VERSION}\" -DNDBUG -O3
 LDFLAGS += ${LIBS}
 DEBUG_CFLAGS = ${CFLAGS} -UNDEBUG -O0 -g -ggdb -Wall -Wextra -Wno-unused-parameter
 CC ?= cc

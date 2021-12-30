cb21d7264919246ca64365cc8a0366a775afe4c4
Paul Chambaz <paul.chambazz@gmail.com>
2021-12-30 21:45:45 +0100

    changed compiler to clang

--- build/config.mk
+++ build/config.mk
@@ -7,5 +7,5 @@ LIBS = -lcurl
 CFLAGS += -std=c17 ${INCS} -DVERSION=\"${VERSION}\" -DNDBUG -O3
 LDFLAGS += ${LIBS}
 DEBUG_CFLAGS = ${CFLAGS} -UNDEBUG -O0 -g -ggdb -Wall -Wextra -Wno-unused-parameter
-CC ?= cc
+CC ?= clang
 STRIP ?= strip
